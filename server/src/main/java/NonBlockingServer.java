592ca1e1707d1c067d57e471592f8a6e37824f14
Christian Reser <christian.reser@ibdeusing.de>
2017-05-19 18:33:43 +0200

    improved naming

---
+++ server/src/main/java/NonBlockingServer.java
@@ -0,0 +1,37 @@
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TNonblockingServerTransport;
+import org.apache.thrift.transport.TTransportException;
+
+
+
+public class NonBlockingServer
+{
+
+    private TServer server;
+    private int port;
+
+    public static void main(String[] argus) throws TTransportException {
+        int port = 4711;
+        NonBlockingServer nonBlockServer = new NonBlockingServer(port);
+        nonBlockServer.start();
+    }
+
+    private void start() {
+        System.out.println("Starting nonblocking server on port " + port);
+        this.server.serve();
+    }
+
+    public NonBlockingServer(int port) throws TTransportException {
+        this.port = port;
+
+        TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(port);
+        ThriftService.Processor<ThriftServiceImpl> processor = new ThriftService.Processor<ThriftServiceImpl>(new ThriftServiceImpl());
+
+        this.server = new TNonblockingServer(new TNonblockingServer.Args(serverTransport).
+                processor(processor));
+
+        System.out.println("Nonblocking server created on port " + port);
+    }
+}
