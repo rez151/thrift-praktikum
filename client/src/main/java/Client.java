57c970cd7538b3508938229f4707a536beef59f0
Christian Reser <christian.reser@ibdeusing.de>
2017-05-19 18:10:54 +0200

    einfach done

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -3,24 +3,54 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 
 public class Client {
 
+    private TTransport transport;
+    private ThriftService.Client client;
+
+
+    public Client(String ip, int port) {
+        transport = new TSocket("192.168.162.128", 4711);
+        TProtocol protocol = new TBinaryProtocol(transport);
+        client = new ThriftService.Client(protocol);
+    }
+
+    private void open() throws TTransportException {
+        this.transport.open();
+    }
+
+    private void close() {
+        this.transport.close();
+    }
+
+    private int add(int zahl1, int zahl2) throws TException {
+        return client.add(zahl1, zahl2);
+    }
+
+    private int multiply(int zahl1, int zahl2) throws TException {
+        return client.multiply(zahl1, zahl2);
+    }
+
+
 
     public static void main(String[] args) throws TException {
-        TTransport transport;
 
+        String ip = "192.168.162.128";
+        int port = 4711;
 
-        transport = new TSocket("localhost", 4712);
-        TProtocol protocol = new TBinaryProtocol(transport);
-        ThriftService.Client client = new ThriftService.Client(protocol);
-        transport.open();
-        int add = client.add(1, 3);
-        int multiply = client.multiply(10, 3);
+        Client easyThriftClient = new Client(ip, port);
+
+        easyThriftClient.open();
+
+        int add = easyThriftClient.add(1, 3);
+        int multiply = easyThriftClient.multiply(10, 3);
 
         System.out.println("add: " + add);
         System.out.println("multiply: " + multiply);
 
-        transport.close();
+        easyThriftClient.close();
     }
+
 }
--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -7,16 +7,30 @@ import org.apache.thrift.transport.TTransportException;
 
 public class Server
 {
-    public static void main(String[] argus) throws TTransportException {
 
-        TServerSocket socket = new TServerSocket(4712);
+    private TServer server;
+    private int port;
+
+    public static void main(String[] argus) throws TTransportException {
+        int port = 4711;
+        Server easyThriftServer = new Server(port);
+        easyThriftServer.start();
+    }
+
+    private void start() {
+        System.out.println("Starting server on port " + port);
+        this.server.serve();
+    }
+
+    public Server(int port) throws TTransportException {
+        this.port = port;
+
+        TServerSocket socket = new TServerSocket(port);
         ThriftService.Iface impl=new ThriftServiceImpl();
         ThriftService.Processor<ThriftService.Iface> processor = new ThriftService.Processor<ThriftService.Iface>(impl);
-
         TThreadPoolServer.Args args = new TThreadPoolServer.Args(socket).processor(processor);
-        TServer server = new TThreadPoolServer(args);
 
-        System.out.println("1: Starting nonblocking server on port 4711 ...");
-        server.serve();
+        this.server = new TThreadPoolServer(args);
+        System.out.println("Server created on port " + port);
     }
 }
